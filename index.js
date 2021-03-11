b282c34253f122fdf06738cc2bcd1db3bfd950f4
GeorgeWu <stupidfive@gmail.com>
2021-03-11 17:53:18 +0800

    Wrap object case

--- index.js
+++ index.js
@@ -36,24 +36,27 @@ function prettifyObject(ast, indentLevel) {
     return pretty
   }
 
-  let pretty = "{"
-  indentLevel++
-  for (let i = 0; i < ast.children.length; i++) {
-    const child = ast.children[i]
-    pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
-    pretty += prettifyObject(child.value, indentLevel)
+  if (ast.type === 'Object') {
+    let pretty = "{"
+    indentLevel++
+    for (let i = 0; i < ast.children.length; i++) {
+      const child = ast.children[i]
+      pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
+      pretty += prettifyObject(child.value, indentLevel)
 
-    const isLast = i === ast.children.length - 1
-    if (!isLast) {
-      pretty += ','
+      const isLast = i === ast.children.length - 1
+      if (!isLast) {
+        pretty += ','
+      }
     }
+    if (ast.children.length !== 0) {
+      pretty += "\n"
+    }
+    indentLevel--
+    pretty += _spaces(indentLevel) + '}'
+    return pretty
   }
-  if (ast.children.length !== 0) {
-    pretty += "\n"
-  }
-  indentLevel--
-  pretty += _spaces(indentLevel) + '}'
-  return pretty
+  throw new Error(`Unrecognized type ${ast.type}`)
 }
 
 function _spaces(indentLevel) {

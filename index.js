8fef24bf8f11d6a225e86cc35fe727f775b14e91
GeorgeWu <stupidfive@gmail.com>
2021-03-11 16:48:54 +0800

    Make recursive

--- index.js
+++ index.js
@@ -10,43 +10,23 @@ export function comment(json) {
   }
 
   const ast = parse(json, settings)
+  let indentLevel = 0
 
-  let pretty = ''
-  pretty += "{"
-  for (let i = 0; i < ast.children.length; i++) {
-    const child = ast.children[i]
-    pretty += '\n  ' + child.key.raw + ': '
-
-    if (child.value.type === 'Literal') {
-      pretty += child.value.raw
-    } else if (child.value.type === 'Object') {
-      pretty += prettifyObject(child.value, 1)
-    }
-
-    const isLast = i === ast.children.length - 1
-    if (!isLast) {
-      pretty += ','
-    }
-  }
-  if (ast.children.length !== 0) {
-    pretty += "\n"
-  }
-  pretty += '}'
-
-  return pretty
+  return prettifyObject(ast, indentLevel)
 }
 
 function prettifyObject(ast, indentLevel) {
   let pretty = ''
   pretty += "{"
+  indentLevel++
   for (let i = 0; i < ast.children.length; i++) {
     const child = ast.children[i]
-    pretty += '\n' + _spaces(indentLevel + 1) + child.key.raw + ': '
+    pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
 
     if (child.value.type === 'Literal') {
       pretty += child.value.raw
     } else if (child.value.type === 'Object') {
-      pretty += prettifyObject(child.value, indentLevel + 1)
+      pretty += prettifyObject(child.value, indentLevel)
     }
 
     const isLast = i === ast.children.length - 1
@@ -57,6 +37,7 @@ function prettifyObject(ast, indentLevel) {
   if (ast.children.length !== 0) {
     pretty += "\n"
   }
+  indentLevel--
   pretty += _spaces(indentLevel) + '}'
   return pretty
 }

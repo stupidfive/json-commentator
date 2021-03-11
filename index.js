2c86f99b63299cd961580a12bbb3dd78c51e25cd
GeorgeWu <stupidfive@gmail.com>
2021-03-11 17:56:54 +0800

    Extract functions

--- index.js
+++ index.js
@@ -4,57 +4,18 @@ export function comment(json) {
   const settings = {
     loc: false,
   }
-
   const ast = parse(json, settings)
   let indentLevel = 0
-
-  return prettifyObject(ast, indentLevel)
+  return _prettify(ast, indentLevel)
 }
 
-function prettifyObject(ast, indentLevel) {
+function _prettify(ast, indentLevel) {
   if (ast.type === 'Literal') {
     return ast.raw
-  }
-  if (ast.type === 'Array') {
-    let pretty = "["
-    indentLevel++
-    for (let i = 0; i < ast.children.length; i++) {
-      const child = ast.children[i]
-      pretty += '\n' + _spaces(indentLevel)
-      pretty += prettifyObject(child, indentLevel)
-
-      const isLast = i === ast.children.length - 1
-      if (!isLast) {
-        pretty += ','
-      }
-    }
-    if (ast.children.length !== 0) {
-      pretty += "\n"
-    }
-    indentLevel--
-    pretty += _spaces(indentLevel) + ']'
-    return pretty
-  }
-
-  if (ast.type === 'Object') {
-    let pretty = "{"
-    indentLevel++
-    for (let i = 0; i < ast.children.length; i++) {
-      const child = ast.children[i]
-      pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
-      pretty += prettifyObject(child.value, indentLevel)
-
-      const isLast = i === ast.children.length - 1
-      if (!isLast) {
-        pretty += ','
-      }
-    }
-    if (ast.children.length !== 0) {
-      pretty += "\n"
-    }
-    indentLevel--
-    pretty += _spaces(indentLevel) + '}'
-    return pretty
+  } else if (ast.type === 'Array') {
+    return _prettifyArray(ast, indentLevel)
+  } else if (ast.type === 'Object') {
+    return _prettyObject(ast, indentLevel)
   }
   throw new Error(`Unrecognized type ${ast.type}`)
 }
@@ -65,4 +26,46 @@ function _spaces(indentLevel) {
     spaces += '  '
   }
   return spaces
+}
+
+function _prettifyArray(ast, indentLevel) {
+  let pretty = "["
+  indentLevel++
+  for (let i = 0; i < ast.children.length; i++) {
+    const child = ast.children[i]
+    pretty += '\n' + _spaces(indentLevel)
+    pretty += _prettify(child, indentLevel)
+
+    const isLast = i === ast.children.length - 1
+    if (!isLast) {
+      pretty += ','
+    }
+  }
+  if (ast.children.length !== 0) {
+    pretty += "\n"
+  }
+  indentLevel--
+  pretty += _spaces(indentLevel) + ']'
+  return pretty
+}
+
+function _prettyObject(ast, indentLevel) {
+  let pretty = "{"
+  indentLevel++
+  for (let i = 0; i < ast.children.length; i++) {
+    const child = ast.children[i]
+    pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
+    pretty += _prettify(child.value, indentLevel)
+
+    const isLast = i === ast.children.length - 1
+    if (!isLast) {
+      pretty += ','
+    }
+  }
+  if (ast.children.length !== 0) {
+    pretty += "\n"
+  }
+  indentLevel--
+  pretty += _spaces(indentLevel) + '}'
+  return pretty
 }

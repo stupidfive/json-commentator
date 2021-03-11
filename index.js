cf9b8627a7ddb7224a7c05369091313e255a2b9c
GeorgeWu <stupidfive@gmail.com>
2021-03-11 16:19:46 +0800

    Add base cases

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+import parse from 'json-to-ast';
+
 export function getOne() {
   return 1
+}
+
+export function comment(json) {
+  const settings = {
+    loc: false,
+  }
+
+  console.log(JSON.stringify(parse(json, settings)));
+
+  const ast = parse(json, settings)
+
+  let pretty = ''
+  pretty += "{"
+  for (const child of ast.children) {
+    pretty += '\n  ' + child.key.raw + ': ' + child.value.raw
+  }
+  if (ast.children.length !== 0) {
+    pretty += "\n"
+  }
+  pretty += '}'
+
+  return pretty
 }
--- test/index.specs.js
+++ test/index.specs.js
@@ -1,10 +1,29 @@
 import chai from 'chai';
-import {getOne} from "../index.js";
+import {comment, getOne} from "../index.js";
 
 let expect = chai.expect;
 
 describe('index', () => {
   it('should return 1 if get 1', () => {
     expect(getOne()).to.equal(1)
+  });
+})
+
+function assertComment(input, expected) {
+  expect(comment(input)).to.equal(expected)
+}
+
+describe('Comment JSON', function () {
+  it('should satisfy base case', () => {
+    assertComment('{}', '{}')
+  });
+  it('should prettify', () => {
+    assertComment(
+        '{"id":123}',
+        '' +
+        '{\n' +
+        '  "id": 123\n' +
+        '}'
+    )
   });
 })

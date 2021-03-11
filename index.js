cab5722b3cadc22daa5e22315b72ab23ddca6671
GeorgeWu <stupidfive@gmail.com>
2021-03-11 19:54:36 +0800

    Print path

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ export function comment(json) {
   return _prettify(ast, indentLevel)
 }
 
-function _prettify(ast, indentLevel) {
+function _prettify(ast, indentLevel, path = []) {
   if (ast.type === 'Literal') {
     return ast.raw
   } else if (ast.type === 'Array') {
-    return _prettifyArray(ast, indentLevel)
+    return _prettifyArray(ast, indentLevel, path)
   } else if (ast.type === 'Object') {
-    return _prettyObject(ast, indentLevel)
+    return _prettyObject(ast, indentLevel, path)
   }
   throw new Error(`Unrecognized type ${ast.type}`)
 }
@@ -28,18 +28,21 @@ function _spaces(indentLevel) {
   return spaces
 }
 
-function _prettifyArray(ast, indentLevel) {
+function _prettifyArray(ast, indentLevel, path) {
   let pretty = "["
   indentLevel++
   for (let i = 0; i < ast.children.length; i++) {
     const child = ast.children[i]
     pretty += '\n' + _spaces(indentLevel)
-    pretty += _prettify(child, indentLevel)
+    let currentPath = [...path, i];
+    pretty += _prettify(child, indentLevel, currentPath)
 
     const isLast = i === ast.children.length - 1
     if (!isLast) {
       pretty += ','
     }
+    // TODO: add comment
+    pretty += ' // ' + currentPath.join('.')
   }
   if (ast.children.length !== 0) {
     pretty += "\n"
@@ -49,18 +52,21 @@ function _prettifyArray(ast, indentLevel) {
   return pretty
 }
 
-function _prettyObject(ast, indentLevel) {
-  let pretty = "{"
+function _prettyObject(ast, indentLevel, path) {
+  let pretty = '{'
   indentLevel++
   for (let i = 0; i < ast.children.length; i++) {
     const child = ast.children[i]
     pretty += '\n' + _spaces(indentLevel) + child.key.raw + ': '
-    pretty += _prettify(child.value, indentLevel)
+    let currentPath = [...path, child.key.value];
+    pretty += _prettify(child.value, indentLevel, currentPath)
 
     const isLast = i === ast.children.length - 1
     if (!isLast) {
       pretty += ','
     }
+    // TODO: add comment
+    pretty += ' // ' + currentPath.join('.')
   }
   if (ast.children.length !== 0) {
     pretty += "\n"
